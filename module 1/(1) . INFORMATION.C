aeb862d5e59732f5db22726658d7112df48163c0
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-07-23 19:33:19 +0530

    Add files via upload

---
+++ module 1/(1) . INFORMATION.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+
+	printf("my name is : Brijesh H Chikhaliya");
+	printf("\nmy age is : 23 year");
+	printf("\nmy birth date is : 10/07/1999");
+	printf("\nmy address is : pranami park , street no.- 2 , rajkot.");
+
+	getch();
+}
